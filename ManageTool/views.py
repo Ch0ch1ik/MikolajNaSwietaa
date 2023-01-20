6c4eaf14a42e4e691aafe2352ce0b01bba1f728d
Ch0ch1ik <chlk777.01@gmail.com>
2023-01-20 09:35:58 +0100

    extracted update orders from index view

--- ManageTool/views.py
+++ ManageTool/views.py
@@ -10,6 +10,7 @@ from django.views import View
 from django.views.generic import UpdateView
 
 from ManageTool.extract import json_extract
+from ManageTool.items_funcs import update_items
 from ManageTool.models import Vs1YkBaformsSubmissions, Order
 
 
@@ -17,9 +18,6 @@ from ManageTool.models import Vs1YkBaformsSubmissions, Order
 @method_decorator(login_required(login_url='/login'), name='dispatch')
 class IndexView(View):
     """Class based view for index page. It shows all orders and allows to assign them to users. """
-
-
-
 
     def get(self, request):
 
@@ -59,154 +57,8 @@ class IndexView(View):
             # for k, v in item:
             #     print(k+'TO'+v)
             # print(json_extract(data, 'title'))
+            update_items(items)
 
-            for item in items:
-
-                new_order, created = Order.objects.get_or_create(bounded_to=item.id)
-                if created:
-                    new_order.type = item.title
-                    new_order.created = item.date_time
-                    if item.message.__contains__('"field_id":"35"' or '"field_id":"226"' or '"field_id":"164"'):
-                        new_order.marketing_approval = True
-                    if item.message.__contains__('"field_id":"36"' or '"field_id":"227"' or '"field_id":"165"'):
-                        new_order.reminder_approval = True
-                    data = json.loads(item.message)
-                    for mess in data:
-                        titles = []
-                        values = []
-                        titles.append(mess['title'])
-                        values.append(mess['message'])
-                        result = dict(zip(titles, values))
-                        for key, value in result.items():
-                            if key == 'Województwo':
-                                new_order.province = value
-                            elif 'Miasto' in key:
-                                new_order.city = value
-                            elif 'Dzielnica' in key:
-                                new_order.district = value
-                            elif key == 'Nazwa placówki':
-                                new_order.facility_name = value
-                            elif key == 'Ulica':
-                                new_order.street = value
-                            elif key == 'Nr domu':
-                                new_order.street_number = value
-                            elif key == 'Nr mieszkania':
-                                new_order.house_number = value
-                            elif key == 'Kod pocztowy':
-                                new_order.zip_code = value
-                            elif key == 'Miejscowość':
-                                new_order.town = value
-                            elif 'Opłata dojazdowa' in key:
-                                fee = 0
-                                if value.__contains__('"84":{}'):
-                                    fee = 0
-                                elif value.__contains__('84":{"3"'):
-                                    fee = 120
-                                elif value.__contains__('84":{"2"'):
-                                    fee = 80
-                                elif value.__contains__('84":{"1"'):
-                                    fee = 40
-                                elif value.__contains__('"196":{}'):
-                                    fee = 0
-                                elif value.__contains__('196":{"3"'):
-                                    fee = 120
-                                elif value.__contains__('196":{"2"'):
-                                    fee = 80
-                                elif value.__contains__('196":{"1"'):
-                                    fee = 40
-                                elif value.__contains__('"257":{}'):
-                                    fee = 0
-                                elif value.__contains__('257":{"3"'):
-                                    fee = 120
-                                elif value.__contains__('257":{"2"'):
-                                    fee = 80
-                                elif value.__contains__('257":{"1"'):
-                                    fee = 40
-                                new_order.arrival_fee = fee
-                            elif key == 'Nazwa firmy':
-                                new_order.company_name = value
-                            elif key == 'Imię i nazwisko zamawiającego':
-                                new_order.name_surname = value
-                            elif key == 'NIP':
-                                new_order.nip = value
-                            elif key == 'Adres siedziby':
-                                new_order.facility_address = value
-                            elif key == 'Telefon kontaktowy':
-                                new_order.phone = value
-                            elif key == 'Adres e-mail':
-                                new_order.email = value
-                            elif key == 'Długość wizyty Mikołaja':
-                                new_order.visit_length = value
-                            elif key == 'Data wizyty':
-                                new_order.visit_date = value
-                            elif key == 'Przedział godzinowy wizyty':
-                                new_order.visit_time = value
-                            elif key == 'Preferowana godzina wizyty':
-                                new_order.pref_visit_time = value
-                            elif key == 'Informacje dodatkowe':
-                                new_order.additional_info = value
-                            elif key == 'Szczegóły zamówienia':
-                                # if value.__contains__('"205":{"2":'):
-                                #     price = 290
-                                #     product = 'Przedszkole WizytaStandard (do 60 min.)'
-                                # else:
-                                #     price = 0
-
-                                new_order.order_details = value
-
-                                # products = (eval(value)['products'])
-                                # cos = json_extract(products, 'title')
-                                # for _ in cos:
-                                #     new_order.products += _+','
-                            elif key == '':
-                                break
-                            else:
-                                print('NIE PYKŁO:', key, value)
-                    new_order.save()
-                else:
-                    pass
-                    # data = json.loads(item.message)
-                    # for mess in data:
-                    #     titles = []
-                    #     values = []
-                    #     titles.append(mess['title'])
-                    #     values.append(mess['message'])
-                    #     result = dict(zip(titles, values))
-                    #     for key, value in result.items():
-                    #         if key == 'Szczegóły zamówienia':
-                    #             true = True
-                    #             false = False
-                    #             # print(eval(value))
-                    #             products = (eval(value)['products'])
-                    #             # print(products)
-                    #             cos = json_extract(products, 'title')
-                    #             print(cos)
-
-                    # for i in data:
-                    #     print(i['message'])
-                    # print(data['products'])
-                    # info = json_extract(data, 'products')
-                    # print(info)
-                    # for mess in data:
-                    #     titles = []
-                    #     values = []
-                    #     titles.append(mess['title'])
-                    #     values.append(mess['message'])
-                    #     result = dict(zip(titles, values))
-                    #     for key, value in result.items():
-                    #         if key == 'Szczegóły zamówienia':
-                    #             true = True
-                    #             false = False
-                    #             for i in list(eval(value)):
-                    #                 if i == 'products':
-                    #                     x = eval(value)[i]
-                    #                     z = []
-                    #                     for y in x:
-                    #                         z.append(y)
-                    #                         print(z)
-
-            # products = Order.objects.all()
-            # print(orders.first().products)
             orders = Order.objects.all()
             for order in orders:
                 if type(order.order_details) == str:
@@ -386,7 +238,6 @@ class Test(View):
 
         return render(request, 'testowy.html', {'orders': orders})
 
-
 # write a test function to test IndexView
 # def test_index_view(self):
 #     # create a request
@@ -429,6 +280,3 @@ class Test(View):
 #     self.assertContains(response, '<td>1</td>')
 #     self.assertContains(response, '<td>2</td>')
 #     self.assertContains(response, '<td>3</td>')
-
-
-
